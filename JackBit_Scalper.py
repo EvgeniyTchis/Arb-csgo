fd6656483f0ac22a92f7a78782d1f5bc2c2a1ef6
EvgeniyTchis <eugenetchistiakov@gmail.com>
2023-03-29 19:02:52 -0700

    JB scalped

---
+++ JackBit_Scalper.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+
+## gets all matches from the bookie
+matchList = requests.get("https://sportservice.inplaynet.tech/api/sport/getheader/en")
+
+## parses all current matches for csgo
+## sport number for csgo is 63 on jackbit
+matchList = json.loads(matchList.json())
+matchList = matchList["EN"]["Sports"]["63"]["Regions"]["820"]["Champs"]
+
+## get all match ids to query
+matchString = ""
+for champ in matchList:
+    for match in matchList[champ]["GameSmallItems"]:
+        matchString = matchString + "," + str(match)
+
+##print(matchString)
+
+## query for odds info
+link = "https://sportservice.inplaynet.tech/api/prematch/getprematchgameall/en/94/?games=" + matchString
+
+matchList = requests.get(link)
+
+print(matchList.json())
+
+
